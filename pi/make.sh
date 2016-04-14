68e0744d11f56957f7e3fe4b0915906936c4fba4
Eric Berquist <eric.berquist@gmail.com>
2016-04-14 13:05:50 -0400

    Start crudely timing things.

--- pi/make.sh
+++ pi/make.sh
@@ -7,8 +7,12 @@ names=(
     pi_omp_reduction
 )
 
+fmt="real: %e user: %U cpu: %P"
+
 for name in ${names[@]}; do
     g++ -fopenmp -o ${name}.x ${name}.cpp
     echo "${name}.x"
-    OMP_NUM_THREADS=4 time ./${name}.x
+    for num_threads in 1 2 4; do
+        OMP_NUM_THREADS=${num_threads} command time -f "$fmt" ./${name}.x
+    done
 done
